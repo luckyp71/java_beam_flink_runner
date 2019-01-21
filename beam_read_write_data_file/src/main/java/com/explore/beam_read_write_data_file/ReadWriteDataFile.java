65d8e1247550532a22277ac905ee7e58f66e83b9
Lucky Pratama <luckypratama71@yahoo.com>
2019-01-21 23:26:59 +0700

    Changing local file system into hdfs

--- beam_read_write_data_file/src/main/java/com/explore/beam_read_write_data_file/ReadWriteDataFile.java
+++ beam_read_write_data_file/src/main/java/com/explore/beam_read_write_data_file/ReadWriteDataFile.java
@@ -1,5 +1,6 @@
 package com.explore.beam_read_write_data_file;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult.State;
 import org.apache.beam.runners.flink.FlinkPipelineOptions;
@@ -9,47 +10,42 @@ import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.ParDo;
-/*
- * Example on how to read and write data from/to a file
- */
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PDone;
+import org.apache.beam.sdk.io.hdfs.HadoopFileSystemOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Example on how to read and write data from/to a file from hdfs
+ */
 
 public class ReadWriteDataFile {
 
-	//A method which configures pipeline for flink runner
-	public static PipelineOptions flinkPipelineOption(String[] args) {
-		PipelineOptions options = PipelineOptionsFactory.fromArgs(args).as(FlinkPipelineOptions.class);
+	// A method which configures pipeline for accessing hdfs and using apache flink runner
+	public static PipelineOptions pipelineOption(String[] args) {
+		HadoopFileSystemOptions options = PipelineOptionsFactory.fromArgs(args).as(FlinkPipelineOptions.class)
+				.as(HadoopFileSystemOptions.class);
+		Configuration conf = new Configuration();
+		conf.set("fs.default.name", "hdfs://localhost:9000");
+		conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+		List<Configuration> list = new ArrayList<Configuration>();
+		list.add(conf);
 		options.setRunner(FlinkRunner.class);
+		options.setHdfsConfiguration(list);
+
 		return options;
 	}
 
-	//Read data from a file
+	// Read data from a file
 	public static PCollection<String> readData(Pipeline pipeline, String path) {
 		PCollection<String> data = pipeline.apply("ReadData", TextIO.read().from(path));
-		return data;
-	}
 
-	//A method to write data to a file
-	public static PDone writeData(PCollection<String> input, String outputPath) {
-		PDone outputData = input.apply(TextIO.write().to(outputPath).withSuffix(".csv"));
-		return outputData;
-	}
-
-	public static void main(String[] args) {
-
-		//Create pipeline
-		Pipeline pipeline = Pipeline.create(flinkPipelineOption(args));
-
-		/*
-		* Read data -> change file path into yours, data must use comma separated like the data 
-		* in the src/main/resource/input.txt (just for example).
-		*/
-		PCollection<String> input = readData(pipeline, "/D://Lucky/Dataset/input.txt");
-
-		//Data processing
-		PCollection<String> words = input.apply(ParDo.of(new DoFn<String, String>() {
+		PCollection<String> words = data.apply(ParDo.of(new DoFn<String, String>() {
 			private static final long serialVersionUID = 1L;
+
 			@ProcessElement
 			public void processElement(ProcessContext c) {
 				String line = c.element();
@@ -59,17 +55,37 @@ public class ReadWriteDataFile {
 				}
 			}
 		}));
-	
-		/* 
-		* Write data -> change file path and name into yours, the path example below will produce file called output.csv 
-		* in /Lucky//dataset/output folder.
-		*/
-		PDone output = writeData(words, "/Lucky//dataset/output");
+
+		return words;
+	}
+
+	// A method to write data to a file
+	public static PDone writeData(PCollection<String> input, String outputPath) {
+		PDone outputData = input.apply(TextIO.write().to(outputPath).withSuffix(".csv"));
+		return outputData;
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		// Creating pipeline
+		Pipeline pipeline = Pipeline.create(pipelineOption(args));
+
+		/*
+		 * Read and process data -> change file path into yours, data must use comma
+		 * separated like the data in the src/main/resource/input.txt (just for
+		 * example).
+		 */
+		PCollection<String> data = readData(pipeline, "hdfs://localhost:9000/data/input.txt");
+
+		/*
+		 * Write data -> change file path and name into yours, the path example below
+		 * will produce file called output.csv in /Lucky//dataset/output folder.
+		 */
+		PDone output = writeData(data, "hdfs://localhost:9000/data/output");
 
 		State result = pipeline.run().waitUntilFinish();
 
-		//Check pipeline status
+		// Check pipeline status
 		System.out.println(result);
-
 	}
 }
